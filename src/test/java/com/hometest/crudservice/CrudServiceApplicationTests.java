452ef1be28b00875430dcd2c17e3d8617930aec1
Surojit <surojit.datta2012@yahoo.in>
2023-12-08 16:27:53 -0800

    First commit

---
+++ src/test/java/com/hometest/crudservice/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hometest.crudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
